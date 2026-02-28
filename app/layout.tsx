c052a9dc40f7436152c7bc7625c9a4e3c290aa4e
Ashish Pise <aspise1817@gmail.com>
2026-02-28 21:54:49 +0530

    modified:   app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from 'next/font/google';
 import Script from 'next/script';
 import './globals.css';
 import Link from 'next/link';
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
